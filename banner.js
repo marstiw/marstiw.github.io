5aab5c2b2a986a7851633795ed4bb7c8b3ef96b6
marstiw <376806830@qq.com>
2025-02-21 16:55:02 +0800

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,5 @@
+[
+    { image: '/assets/images/banner.png', url: '/pages/detail/detail?id=1' },
+    { image: '/assets/images/banner.png', url: '/pages/detail/detail?id=1' },
+    { image: '/assets/images/banner.png', url: '/pages/detail/detail?id=1' }
+]
